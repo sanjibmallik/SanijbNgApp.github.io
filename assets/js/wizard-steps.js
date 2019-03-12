d90b2eb373a0363e76978086ac9a7e326e3b0c00
Sanjib Mallik <Sanjibmallik.developer@gmail.com>
2019-03-12 15:46:45 +0530

    base file commited

---
+++ assets/js/wizard-steps.js
@@ -0,0 +1,15 @@
+
+$(document).ready( function(){
+    $(".icons-tab-steps").steps({
+        headerTag: "h6",
+        bodyTag: "fieldset",
+        transitionEffect: "fade",
+        titleTemplate: '<span class="step">#index#</span> #title#',
+        labels: {
+            finish: 'Submit'
+        },
+        onFinished: function (event, currentIndex) {
+            alert("Form submitted.");
+        }
+    });
+ });
